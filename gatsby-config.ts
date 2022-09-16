9f3fb1835ddc5f90831194753e7e7f3354d847e3
dabin0127 <duekthtjf783@gmail.com>
2022-09-16 22:18:54 +0900

    🔧 chore: Gatsby config 및 tsconfig 초기화

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,10 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from 'gatsby';
+
+import siteMetaConfig from './site-meta-config';
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `uparupa-and-me`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
+  siteMetadata: siteMetaConfig,
   plugins: [],
-}
+};
 
-export default config
+export default config;
---
+++ site-meta-config.ts
@@ -0,0 +1,14 @@
+const SiteMetaConfig = {
+  title: `Uparupa and ME`,
+  subtitle: `존귀하고 귀여우신 우파루파님과 그냥 다빈`,
+  description: `안녕하세요. 우파루파 좋아하세요?`,
+  author: {
+    name: `Kim Dabin`,
+  },
+  introduction: `안녕하세요. 귀여운 우파루파가 이렇게 많으니 행복하군요.`,
+  social: {
+    github: `upa-r-upa`,
+  },
+};
+
+export default SiteMetaConfig;
